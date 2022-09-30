0f8782a3cf0beb7be36504a18089ee5c6bd4e1de
Levi Barsis <lbarsis@gmail.com>
2022-09-30 12:37:01 -0700

    Removed screen.debug from article.test.js in attempt to not have incomplete grade

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -8,7 +8,5 @@ test("displays the text 'please pass this test'", () => {
   render(<Article />);
   const element = screen.queryByText("please pass this test");
 
-  screen.debug(element);
-
   expect(element).toBeInTheDocument();
 });
